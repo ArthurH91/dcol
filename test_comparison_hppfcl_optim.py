ac5371e2ce1c0790ce7606b02884dbed6546dbe8
arthur <arthur.haffemayer@yahoo.fr>
2024-08-26 19:12:13 +0200

    1e-4 of error between gjk and optim for cps

--- test_comparison_hppfcl_optim.py
+++ test_comparison_hppfcl_optim.py
@@ -43,16 +43,10 @@ class TestDistOpt(unittest.TestCase):
         cls.qcqp_solver.solve_problem(warm_start_primal=np.concatenate((cls.c1, cls.c2)))
 
         cls.x1, cls.x2 = cls.qcqp_solver.get_optimal_values()
-        cls.distance = cls.qcqp_solver.get_minimum_cost()
+        # cls.distance = cls.qcqp_solver.get_minimum_cost()
         cls.cp1, cls.cp2 = cp_hppfcl(cls.shape1, cls.c1_SE3, cls.shape2, cls.c2_SE3)
-        print(cls.c1)
-        print(cls.c2)
-        print(cls.center)
         cls.distance_v2 = cls.func_distance_annalytical(cls.center)
         
-        
-        
-                
     @classmethod
     def func_distance_annalytical(cls, center):
         center_1 = center[:3]
@@ -61,8 +55,6 @@ class TestDistOpt(unittest.TestCase):
         qcqp_solver.setup_problem(center_1, cls.A1, center_2, cls.A2)
         qcqp_solver.solve_problem(warm_start_primal=center)
         x1, x2 = qcqp_solver.get_optimal_values()
-        print(x1)
-        print(x2)
         return np.linalg.norm(x1 - x2, 2)
     
     
@@ -90,23 +82,27 @@ class TestDistOpt(unittest.TestCase):
             msg = f"The closest point 2 computed from GJK ({cls.cp2})is not the same as the one computed with the QCQP ({cls.x2})."
         )
 
+
+
 def dist_hppfcl(shape1, c1, shape2, c2):
     
     req = hppfcl.DistanceRequest()
     res = hppfcl.DistanceResult()
-    
+    req.gjk_max_iterations = 20000
+    req.abs_err = 0
+    req.gjk_tolerance = 1e-9
     return hppfcl.distance(shape1, c1, shape2, c2,req, res)
 
 def cp_hppfcl(shape1, c1, shape2, c2):
     
     req = hppfcl.DistanceRequest()
     res = hppfcl.DistanceResult()
-    
+    req.gjk_max_iterations = 20000
+    req.abs_err = 0
+    req.gjk_tolerance = 1e-9    
     _ =  hppfcl.distance(shape1, c1, shape2, c2,req, res)
-
     cp1 = res.getNearestPoint1()
     cp2 = res.getNearestPoint2()
-    
     return cp1, cp2
 
 if __name__ == "__main__":
