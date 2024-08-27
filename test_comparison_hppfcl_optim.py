13b7c76856df34f4125e44269540d1118ad46472
arthur <arthur.haffemayer@yahoo.fr>
2024-08-27 15:03:54 +0200

    cleaning the unit test but it's broken again

--- test_comparison_hppfcl_optim.py
+++ test_comparison_hppfcl_optim.py
@@ -5,105 +5,191 @@ import hppfcl
 import pinocchio as pin
 
 from ellipsoid_optimization import EllipsoidOptimization
+from wrapper_panda import PandaWrapper
+from distance_derivatives import dist, cp
+from viewer import create_viewer, add_sphere_to_viewer
 
-class TestDistOpt(unittest.TestCase):
+pin.seed(1)
+np.random.seed(29)
+
+class TestComparisonDistOpt(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
 
+        ###! Definition of the shapes
         # Define initial positions for the centers of the two ellipsoids
-        cls.c1 = np.random.randn(3)
-        cls.c2 = 10 * np.random.randn(3) + 10
-        cls.center = np.concatenate((cls.c1, cls.c2))
+        cls.c1_init = np.random.randn(3) 
+        cls.c2_init = np.zeros(3)
 
+        # Define initial rotation for the ellipsoids
+        cls.R1_init = pin.SE3.Random().rotation
+        cls.R2_init = pin.SE3.Random().rotation
         # Define the radii for the ellipsoids
-        cls.radiiA = [1, 2, 1]
-        cls.radiiB = [1, 1, 2]
+        cls.radii1 = [0.1, 0.2, 0.1]
+        cls.radii2 = [0.1, 0.1, 0.2]
 
-        cls.shape1 = hppfcl.Ellipsoid(*cls.radiiA)
-        cls.shape2 = hppfcl.Ellipsoid(*cls.radiiB)
+        cls.shape1 = hppfcl.Ellipsoid(*cls.radii1)
+        cls.shape2 = hppfcl.Ellipsoid(*cls.radii2)
 
         # Construct matrices A and B for ellipsoid constraints
-        D1 = np.diag([1 / r**2 for r in cls.radiiA])
-        D2 = np.diag([1 / r**2 for r in cls.radiiB])
+        cls.D1 = np.diag([1 / r**2 for r in cls.radii1])
+        cls.D2 = np.diag([1 / r**2 for r in cls.radii2])
 
-        # Generate random rotation matrices using Pinocchio
-        cls.R_A = pin.SE3.Random().rotation
-        cls.R_B = pin.SE3.Random().rotation
+        ###! Creation of the robot 
+        # OBS CONSTANTS
+        cls.PLACEMENT_OBS = pin.SE3(cls.R1_init, cls.c1_init)
+        cls.DIM_OBS = cls.radii1
 
-        # Calculate rotated matrices
-        cls.A1 = cls.R_A.T @ D1 @ cls.R_A
-        cls.A2 = cls.R_B.T @ D2 @ cls.R_B
+        # ELLIPS ON THE ROBOT
+        cls.PLACEMENT_ROB = pin.SE3(cls.R2_init, cls.c2_init)
+        cls.DIM_ROB = cls.radii2
 
-        cls.c1_SE3 = pin.SE3(rotation=cls.R_A.T, translation=cls.c1)
-        cls.c2_SE3 = pin.SE3(rotation=cls.R_B.T, translation=cls.c2)
+        # Creating the robot
+        robot_wrapper = PandaWrapper()
+        cls.rmodel, cls.cmodel, cls.vmodel = robot_wrapper()
 
-        cls.qcqp_solver = EllipsoidOptimization()
-        cls.qcqp_solver.setup_problem(cls.c1, cls.A1, cls.c2, cls.A2)
-        cls.qcqp_solver.solve_problem(warm_start_primal=np.concatenate((cls.c1, cls.c2)))
+        cls.cmodel = robot_wrapper.add_2_ellips(
+            cls.cmodel,
+            placement_obs=cls.PLACEMENT_OBS,
+            dim_obs=cls.DIM_OBS,
+            placement_rob=cls.PLACEMENT_ROB,
+            dim_rob=cls.DIM_ROB,
+        )
+        # Creating the data models
+        cls.rdata = cls.rmodel.createData()
+        cls.cdata = cls.cmodel.createData()
 
-        cls.x1, cls.x2 = cls.qcqp_solver.get_optimal_values()
-        # cls.distance = cls.qcqp_solver.get_minimum_cost()
-        cls.cp1, cls.cp2 = cp_hppfcl(cls.shape1, cls.c1_SE3, cls.shape2, cls.c2_SE3)
-        cls.distance_v2 = cls.func_distance_annalytical(cls.center)
+        cls.viz = create_viewer(cls.rmodel, cls.cmodel, cls.vmodel)
+
         
     @classmethod
-    def func_distance_annalytical(cls, center):
-        center_1 = center[:3]
-        center_2 = center[3:]
-        qcqp_solver = EllipsoidOptimization()
-        qcqp_solver.setup_problem(center_1, cls.A1, center_2, cls.A2)
-        qcqp_solver.solve_problem(warm_start_primal=center)
-        x1, x2 = qcqp_solver.get_optimal_values()
-        return np.linalg.norm(x1 - x2, 2)
+    def update_A_R_c(cls, q,v = None):
+        
+        pin.updateGeometryPlacements(cls.rmodel, cls.rdata, cls.cmodel, cls.cdata, q)
+        pin.framesForwardKinematics(cls.rmodel, cls.rdata, q)
+        if v is not None:
+            pin.forwardKinematics(cls.rmodel, cls.rdata, q, v)
+        
+        R1 = cls.cdata.oMg[cls.cmodel.getGeometryId("obstacle")].rotation
+        R2 = cls.cdata.oMg[cls.cmodel.getGeometryId("ellips_rob")].rotation
+
+        c1 = cls.cdata.oMg[cls.cmodel.getGeometryId("obstacle")].translation
+        c2 = cls.cdata.oMg[cls.cmodel.getGeometryId("ellips_rob")].translation
+
+        # # Calculate rotated matrices
+        A1 = R1.T @ cls.D1 @ R1
+        A2 = R2.T @ cls.D2 @ R2
+        
+        return A1, A2, R1, R2, c1, c2
     
-    
-    def test_dist(cls):
+    @classmethod
+    def setup_random_config(cls):
+        
+        q = pin.randomConfiguration(cls.rmodel)
+        v = pin.randomConfiguration(cls.rmodel)
+        x = np.concatenate((q, v))
+
+        A1, A2, R1, R2, c1, c2 = cls.update_A_R_c(q, v)
+        
+        c1_SE3 = pin.SE3(R1.T, c1)
+        c2_SE3 = pin.SE3(R2.T, c2)
+
+        A1_SE3 = pin.SE3(A1.T, c1)
+        A2_SE3 = pin.SE3(A2.T, c2)
+
+        cls.cp1_hppfcl_without_robot, cls.cp2_hppfcl_without_robot = cp_hppfcl(cls.shape1, c1_SE3, cls.shape2, c2_SE3)
+        cls.dist_hppfcl_without_robot = dist_hppfcl(cls.shape1, c1_SE3, cls.shape2, c2_SE3)
+        cls.dist_opt_val = dist_opt(cls.shape1, c1_SE3, cls.shape2, c2_SE3)
+        cls.cp1_opt_val, cls.cp2_opt_val = cp_opt(cls.shape1, c1_SE3, cls.shape2, c2_SE3)
+
+        # ###! Vizualisation
+        add_sphere_to_viewer(cls.viz, "cp1_hppfcl_without_robot", 1e-2, cls.cp1_hppfcl_without_robot)
+        add_sphere_to_viewer(cls.viz, "cp2_hppfcl_without_robot", 1e-2, cls.cp2_hppfcl_without_robot)
+
+        add_sphere_to_viewer(cls.viz, "cp1_opt", 2e-2, cls.cp1_opt_val)
+        add_sphere_to_viewer(cls.viz, "cp2_opt", 2e-2, cls.cp2_opt_val)
+
+        cls.viz.display(q)        
+
+    def test_cp1_random_config(cls):
+        cls.setup_random_config()
+
         cls.assertAlmostEqual(
-            np.linalg.norm(dist_hppfcl(cls.shape1, cls.c1_SE3, cls.shape2, cls.c2_SE3)- cls.distance_v2),
+            np.linalg.norm(cls.cp1_hppfcl_without_robot - cls.cp1_opt_val),
             0,
-            places=6,
-            msg = f"The distance computed from GJK ({dist_hppfcl(cls.shape1, cls.c1_SE3, cls.shape2, cls.c2_SE3)})is not the same as the distance computed with the QCQP ({cls.distance_v2})."
+            places=9,
+            msg=f"The closest point 1 computed from GJK ({cls.cp1_hppfcl_without_robot})is not the same as the one computed with the QCQP ({cls.cp1_opt_val}).",
         )
-    
-    def test_cp1(cls):
+        
+    def test_cp2_random_config(cls):
+        cls.setup_random_config()
         cls.assertAlmostEqual(
-            np.linalg.norm(cls.cp1 - cls.x1),
+            np.linalg.norm(cls.cp2_hppfcl_without_robot - cls.cp2_opt_val),
             0,
-            places=4,
-            msg = f"The closest point 1 computed from GJK ({cls.cp1})is not the same as the one computed with the QCQP ({cls.x1})."
+            places=9,
+            msg=f"The closest point 2 computed from GJK ({cls.cp2_hppfcl_without_robot})is not the same as the one computed with the QCQP ({cls.cp2_opt_val}).",
         )
 
-    def test_cp2(cls):
+    def test_dist_random_config(cls):
+        
+        cls.setup_random_config()
         cls.assertAlmostEqual(
-            np.linalg.norm(cls.cp2 - cls.x2),
+            np.linalg.norm(
+                 cls.dist_hppfcl_without_robot
+                - cls.dist_opt_val
+            ),
             0,
-            places=4,
-            msg = f"The closest point 2 computed from GJK ({cls.cp2})is not the same as the one computed with the QCQP ({cls.x2})."
+            places=5,
+            msg=f"The distance computed from GJK ({cls.dist_hppfcl_without_robot})is not the same as the distance computed with the QCQP ({cls.dist_opt_val}).",
         )
 
+def dist_hppfcl(shape1, c1_se3, shape2, c2_se3):
 
-
-def dist_hppfcl(shape1, c1, shape2, c2):
-    
     req = hppfcl.DistanceRequest()
     res = hppfcl.DistanceResult()
     req.gjk_max_iterations = 20000
     req.abs_err = 0
     req.gjk_tolerance = 1e-9
-    return hppfcl.distance(shape1, c1, shape2, c2,req, res)
+    return hppfcl.distance(shape1, c1_se3, shape2, c2_se3, req, res)
 
-def cp_hppfcl(shape1, c1, shape2, c2):
-    
+
+def cp_hppfcl(shape1, c1_se3, shape2, c2_se3):
+
     req = hppfcl.DistanceRequest()
     res = hppfcl.DistanceResult()
     req.gjk_max_iterations = 20000
     req.abs_err = 0
-    req.gjk_tolerance = 1e-9    
-    _ =  hppfcl.distance(shape1, c1, shape2, c2,req, res)
+    req.gjk_tolerance = 1e-9
+    _ = hppfcl.distance(shape1, c1_se3, shape2, c2_se3, req, res)
     cp1 = res.getNearestPoint1()
     cp2 = res.getNearestPoint2()
     return cp1, cp2
+
+def cp_opt(shape1, c1_se3, shape2, c2_se3):
+    
+    D1 = np.diag([1 / r**2 for r in shape1.radii])
+    D2 = np.diag([1 / r**2 for r in shape2.radii])
+    
+    R1 = c1_se3.rotation
+    R2 = c2_se3.rotation
+
+    A1 = R1.T @ D1 @ R1
+    A2 = R2.T @ D2 @ R2
+
+    qcqp_solver = EllipsoidOptimization()
+    qcqp_solver.setup_problem(c1_se3.translation, A1, c2_se3.translation, A2)
+    qcqp_solver.solve_problem(
+        warm_start_primal=np.concatenate((c1_se3.translation, c2_se3.translation))
+    )
+
+    cp1, cp2 = qcqp_solver.get_optimal_values()
+    return cp1,cp2
+
+def dist_opt(shape1, c1_se3, shape2, c2_se3):
+    x1,x2 = cp_opt(shape1, c1_se3, shape2, c2_se3)
+    return np.linalg.norm(x1-x2,2)
+
 
 if __name__ == "__main__":
     unittest.main()
