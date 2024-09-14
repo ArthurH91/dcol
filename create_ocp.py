3e76ea2319eef1ab4ff71ca55b4c78769ae66370
arthur <arthur.haffemayer@yahoo.fr>
2024-09-14 16:19:37 +0200

    add the no col in ocp

--- create_ocp.py
+++ create_ocp.py
@@ -27,7 +27,7 @@ def create_ocp_velocity(rmodel, gmodel, scene):
     framePlacementResidual = crocoddyl.ResidualModelFrameTranslation(
         state,
         rmodel.getFrameId("panda2_rightfinger"),
-        np.array(scene['TARGET_POSE']['position']),
+        np.array(scene['TARGET_POSE']['translation']),
     )
 
     goalTrackingCost = crocoddyl.CostModelResidual(
@@ -147,7 +147,7 @@ def create_ocp_distance(rmodel, gmodel, scene):
     framePlacementResidual = crocoddyl.ResidualModelFrameTranslation(
         state,
         rmodel.getFrameId("panda2_rightfinger"),
-        np.array(scene['TARGET_POSE']['position']),
+        np.array(scene['TARGET_POSE']['translation']),
     )
 
     goalTrackingCost = crocoddyl.CostModelResidual(
@@ -208,6 +208,95 @@ def create_ocp_distance(rmodel, gmodel, scene):
         actuation,
         terminalCostModel,
         terminalConstraintModelManager,
+    )
+
+    runningModel = crocoddyl.IntegratedActionModelEuler(
+        running_DAM, scene["dt"]
+    )
+    terminalModel = crocoddyl.IntegratedActionModelEuler(
+        terminal_DAM, 0.0
+    )
+
+    # runningModel.differential.armature = np.array(
+    #     [0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.0]
+    # )
+    # terminalModel.differential.armature = np.array(
+    #     [0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.0]
+    # )
+
+    problem = crocoddyl.ShootingProblem(
+        np.r_[scene['INITIAL_CONFIG'], scene['INITIAL_VELOCITY']], [runningModel] * scene["T"], terminalModel
+    )
+    # Create solver + callbacks
+    # Define mim solver with inequalities constraints
+    ocp = mim_solvers.SolverCSQP(problem)
+
+    # Merit function
+    ocp.use_filter_line_search = False
+
+    # Parameters of the solver
+    ocp.termination_tolerance = 1e-3
+    ocp.max_qp_iters = 10000
+    ocp.eps_abs = 1e-6
+    ocp.eps_rel = 0
+
+    ocp.with_callbacks = True
+    
+    return ocp
+
+
+
+def create_ocp_nocol(rmodel, gmodel, scene):
+    # Stat and actuation model
+    state = crocoddyl.StateMultibody(rmodel)
+    actuation = crocoddyl.ActuationModelFull(state)
+
+    # Running & terminal cost models
+    runningCostModel = crocoddyl.CostModelSum(state)
+    terminalCostModel = crocoddyl.CostModelSum(state)
+
+    ### Creation of cost terms
+
+    # State Regularization cost
+    xResidual = crocoddyl.ResidualModelState(state, np.r_[scene['INITIAL_CONFIG'], scene['INITIAL_VELOCITY']])
+    xRegCost = crocoddyl.CostModelResidual(state, xResidual)
+
+    # Control Regularization cost
+    uResidual = crocoddyl.ResidualModelControl(state)
+    uRegCost = crocoddyl.CostModelResidual(state, uResidual)
+
+    # End effector frame cost
+    framePlacementResidual = crocoddyl.ResidualModelFrameTranslation(
+        state,
+        rmodel.getFrameId("panda2_rightfinger"),
+        np.array(scene['TARGET_POSE']['translation']),
+    )
+
+    goalTrackingCost = crocoddyl.CostModelResidual(
+        state, framePlacementResidual
+    )
+
+    # Adding costs to the models
+    runningCostModel.addCost("stateReg", xRegCost, scene["WEIGHT_xREG"])
+    runningCostModel.addCost("ctrlRegGrav", uRegCost, scene["WEIGHT_uREG"])
+    runningCostModel.addCost(
+        "gripperPoseRM", goalTrackingCost, scene["WEIGHT_GRIPPER_POSE"]
+    )
+    terminalCostModel.addCost("stateReg", xRegCost, scene["WEIGHT_xREG"])
+    terminalCostModel.addCost(
+        "gripperPose", goalTrackingCost, scene["WEIGHT_GRIPPER_POSE_TERM"]
+    )
+
+    # Create Differential Action Model (DAM), i.e. continuous dynamics and cost functions
+    running_DAM = crocoddyl.DifferentialActionModelFreeFwdDynamics(
+        state,
+        actuation,
+        runningCostModel,
+    )
+    terminal_DAM = crocoddyl.DifferentialActionModelFreeFwdDynamics(
+        state,
+        actuation,
+        terminalCostModel,
     )
 
     runningModel = crocoddyl.IntegratedActionModelEuler(
