afeeef9820948be7c281c5dde501fcf1802bb6ee
arthur <arthur.haffemayer@yahoo.fr>
2024-09-14 14:50:01 +0200

    scene 1 finished

--- simple_ocp.py
+++ simple_ocp.py
@@ -7,10 +7,7 @@ from viewer import create_viewer, add_sphere_to_viewer, add_cube_to_viewer
 
 np.set_printoptions(precision=4, linewidth=350, suppress=True, threshold=1e6)
 import yaml
-from create_ocp import create_ocp
-
-### PARAMETERS
-# Number of nodes of the trajectory
+from create_ocp import create_ocp_velocity, create_ocp_distance
 
 # Load variables from the YAML file
 with open("config_scenes.yaml", "r") as file:
@@ -32,8 +29,8 @@ rmodel, gmodel, vmodel = robot_wrapper()
 gmodel.removeGeometryObject("panda1_box_0")
 vmodel.removeGeometryObject("panda1_box_0")
 
-radii1 = scene["DIM_OBS"]["radii1"]
-radii2 = scene["DIM_ROB"]["radii1"]
+radii1 = scene["DIM_OBS"][0]
+radii2 = scene["DIM_ROB"][0]
 
 
 Mobs = pin.SE3(
@@ -71,21 +68,49 @@ for col_pair in scene["collision_pairs"]:
     )
 
 
-#### Creating the OCP
-ocp = create_ocp(rmodel, gmodel, scene)
+#### Creating the OCP for the velocity
+ocp_vel = create_ocp_velocity(rmodel, gmodel, scene)
 XS_init = [np.r_[scene["INITIAL_CONFIG"], scene["INITIAL_VELOCITY"]]] * (scene["T"] + 1)
-US_init = ocp.problem.quasiStatic(XS_init[:-1])
+US_init = ocp_vel.problem.quasiStatic(XS_init[:-1])
 
 #### Solving the OCP
-ocp.solve(XS_init, US_init, 100)
+ocp_vel.solve(XS_init, US_init, 100)
+
+#### Creating the OCP for the distance
+ocp_dist = create_ocp_distance(rmodel, gmodel, scene)
+ocp_dist.solve(XS_init,US_init, 100)
 
 #### Creating the visualizer
 viz = create_viewer(rmodel, gmodel, vmodel)
 add_sphere_to_viewer(viz, "goal", 5e-2, TARGET_POSE.translation, color=100000)
 
+#### Displaying the trajectories
+
+for i, xs in enumerate(ocp_vel.xs):
+        q = np.array(xs[:7].tolist())
+        pin.framesForwardKinematics(rmodel, rdata, q)
+        add_cube_to_viewer(
+            viz,
+            "vcolmpc" + str(i),
+            [2e-2, 2e-2, 2e-2],
+            rdata.oMf[rmodel.getFrameId("panda2_rightfinger")].translation,
+            color=100000000,
+        )
+
+for i, xs in enumerate(ocp_dist.xs):
+        q = np.array(xs[:7].tolist())
+        pin.framesForwardKinematics(rmodel, rdata, q)
+        add_sphere_to_viewer(
+            viz,
+            "colmpc" + str(i),
+            2e-2,
+            rdata.oMf[rmodel.getFrameId("panda2_rightfinger")].translation,
+            color=100000,
+        )
+
 #### Displaying the solution
 while True:
-    for i, xs in enumerate(ocp.xs):
+    for i, xs in enumerate(ocp_vel.xs):
         q = np.array(xs[:7].tolist())
         pin.framesForwardKinematics(rmodel, rdata, q)
         add_cube_to_viewer(
